15c17840f2eeb495264a8b027e1831d862fb2986
Agastya Patri <patriagastya5@gmail.com>
2024-12-16 21:14:59 +0530

    barbar added

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -43,20 +43,19 @@ return {
 		}
 	},
 	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- add any options here
-		},
+		'romgrk/barbar.nvim',
 		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-		}
-	},
-
+		  'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+		  'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+		},
+		init = function() vim.g.barbar_auto_setup = false end,
+		opts = {
+		  -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+		  -- animation = true,
+		  -- insert_at_start = true,
+		  -- …etc.
+		},
+		version = '^1.0.0', -- optional: only update when a new 1.x version is released
+	}
 
 }
